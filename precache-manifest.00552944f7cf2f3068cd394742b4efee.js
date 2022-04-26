bb7b75e76cd3ae0e4b5cb05da1c592648b8bb016
simkati <simkat@gmx.net>
2022-04-26 12:25:17 +0200

    Updates

---
+++ precache-manifest.00552944f7cf2f3068cd394742b4efee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a1bd62a0357cdfda2a",
+    "url": "/wordy/static/css/main.c469d89a.chunk.css"
+  },
+  {
+    "revision": "83a1bd62a0357cdfda2a",
+    "url": "/wordy/static/js/main.7420799b.chunk.js"
+  },
+  {
+    "revision": "cf6d7d0f8fe0f4a1ba57",
+    "url": "/wordy/static/js/runtime~main.d309cac2.js"
+  },
+  {
+    "revision": "b2d2ca53ebd07e32fe05",
+    "url": "/wordy/static/js/2.022cc035.chunk.js"
+  },
+  {
+    "revision": "77ff2abf6da91ad83241",
+    "url": "/wordy/static/js/3.153d6254.chunk.js"
+  },
+  {
+    "revision": "b4b4922481428c23bbe302caee7b8d47",
+    "url": "/wordy/index.html"
+  }
+];
